022474f890fe90c8181115aab6c78a2176696dd0
Ajithhh0 <88576273+Ajithhh0@users.noreply.github.com>
2023-09-02 12:45:36 +0530

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ from jsonschema import ValidationError
 app = Flask(__name__) 
 load_dotenv()
 
-# Define the JSON schema for the request body
+
 schema = {
     "type": "object",
     "properties": {
@@ -26,8 +26,7 @@ schema = {
 @expects_json(schema)
 def convert_currency():
     try:
-        # Validate the request body against the schema
-        #jsonschema.validate(request.get_json(), schema)
+        
 
         data = request.get_json()
         print(data)
@@ -36,16 +35,13 @@ def convert_currency():
         to_currency = data['to_currency']
         Date = data.get('date')
 
-        # Optional date parameter
         if Date:
-            # Get the exchange rates for the specified currencies and date
             historical_data = yf.download(from_currency+to_currency+'=X', start="2004-01-01", end=date)
             if not historical_data.empty:
                 rate = historical_data['Close'][0]
             else:
                 return "Error getting historical exchange rates"
         else:
-            # Get the latest exchange rate for the specified currencies
             latest_data = yf.download(from_currency+to_currency+'=X', period='1d')
             if not latest_data.empty:
               rate = latest_data['Close'][0]
@@ -55,7 +51,6 @@ def convert_currency():
         converted_amount = amount * rate
         return jsonify({"result": converted_amount})
     except jsonschema.exceptions.ValidationError as e:
-        # Return a 400 Bad Request if the request body doesn't match the schema
         return f"Invalid request: {e.message}", 400
     except Exception as e:
         print(e)       
